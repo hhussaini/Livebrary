8c8af12a3fc37c1c1fd447fe1e155275bbe140aa
paul-man <paul.mannarino@stonybrook.edu>
2016-02-27 02:54:10 -0500

    Home page is getting there, basic idea in place

--- GoodLooksBooks-Deliverable2/js/bootstrap.js
+++ GoodLooksBooks-Deliverable2/js/bootstrap.js
@@ -4,62 +4,6 @@
  * Licensed under the MIT license
  */
 
-// var jumboHeight = $('.jumbotron-header').outerHeight();
-// function parallax(){
-//     var scrolled = $(window).scrollTop();
-//     $('.bg-jumbo').css('height', (jumboHeight-scrolled) + 'px');
-// }
-//
-// $(window).scroll(function(e){
-//     parallax();
-// });
-(function($) {
-
-    $.fn.parallax = function(options) {
-
-        var windowHeight = $(window).height();
-
-        // Establish default settings
-        var settings = $.extend({
-            speed        : 0.15
-        }, options);
-
-        // Iterate over each object in collection
-        return this.each( function() {
-
-        	// Save a reference to the element
-        	var $this = $(this);
-
-        	// Set up Scroll Handler
-        	$(document).scroll(function(){
-
-    		        var scrollTop = $(window).scrollTop();
-            	        var offset = $this.offset().top;
-            	        var height = $this.outerHeight();
-
-    		// Check if above or below viewport
-			if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-				return;
-			}
-
-			var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
-
-                 // Apply the Y Background Position to Set the Parallax Effect
-    			$this.css('background-position', 'center ' + yBgPosition + 'px');
-
-        	});
-        });
-    }
-}(jQuery));
-
-$('.bg-1,.bg-3').parallax({
-	speed :	0.15
-});
-
-$('.bg-2').parallax({
-	speed :	0.25
-});
-
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
